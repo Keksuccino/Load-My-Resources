bdc07ab87cc0de0508e45f13d0db0fa1cb3ffe40
Keksuccino <35544624+Keksuccino@users.noreply.github.com>
2022-11-11 01:11:50 +0100

    Release v1.0.2

--- src/main/java/de/keksuccino/loadmyresources/LoadMyResources.java
+++ src/main/java/de/keksuccino/loadmyresources/LoadMyResources.java
@@ -3,6 +3,7 @@ package de.keksuccino.loadmyresources;
 import de.keksuccino.loadmyresources.pack.LMRFolderResourcePack;
 import de.keksuccino.loadmyresources.pack.PackHandler;
 import de.keksuccino.loadmyresources.utils.config.Config;
+import net.minecraft.client.Minecraft;
 import net.minecraftforge.fml.client.FMLClientHandler;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.relauncher.Side;
@@ -14,9 +15,9 @@ import java.io.File;
 @Mod(modid = "loadmyresources", acceptedMinecraftVersions="[1.12,1.12.2]", dependencies = "required:forge@[14.23.5.2855,]", clientSideOnly = true)
 public class LoadMyResources {
 
-    public static final String VERSION = "1.0.1";
-    
-    public static final File HOME_DIR = new File("config/loadmyresources/");
+    public static final String VERSION = "1.0.2";
+
+    public static final File HOME_DIR = new File(Minecraft.getMinecraft().mcDataDir, "config/loadmyresources/");
 
     public static final Logger LOGGER = LogManager.getLogger();
 
@@ -55,7 +56,7 @@ public class LoadMyResources {
 
         try {
 
-            config = new Config(HOME_DIR.getPath() + "/config.cfg");
+            config = new Config(HOME_DIR.getAbsolutePath() + "/config.cfg");
 
             //---------------------
 
--- src/main/java/de/keksuccino/loadmyresources/pack/PackHandler.java
+++ src/main/java/de/keksuccino/loadmyresources/pack/PackHandler.java
@@ -17,12 +17,12 @@ public class PackHandler {
 //    public static final String PACK_DESCRIPTION = "LMR Resources";
     public static final ResourceLocation DUMMY_PACK_META = new ResourceLocation("loadmyresources", "dummy.pack.mcmeta");
 
-    public static File resourcesDirectory = new File("resources/");
+    public static File resourcesDirectory = new File(Minecraft.getMinecraft().mcDataDir, "resources/");
 
     public static void init() {
 
         if (LoadMyResources.config != null) {
-            resourcesDirectory = new File(LoadMyResources.config.getOrDefault("resource_path", "resources/"));
+            resourcesDirectory = new File(Minecraft.getMinecraft().mcDataDir, LoadMyResources.config.getOrDefault("resource_path", "resources/"));
         } else {
             LoadMyResources.LOGGER.error("[LOAD MY RESOURCES] ERROR: Config not loaded! Can't get resource path! Path set to default 'resources/'!");
         }
