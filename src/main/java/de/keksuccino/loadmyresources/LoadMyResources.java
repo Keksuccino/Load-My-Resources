5f0490c608ae37ffeef197fc5b7c3dabe44af891
Keksuccino <35544624+Keksuccino@users.noreply.github.com>
2022-11-11 01:10:30 +0100

    Release v1.0.2

--- src/main/java/de/keksuccino/loadmyresources/LoadMyResources.java
+++ src/main/java/de/keksuccino/loadmyresources/LoadMyResources.java
@@ -3,6 +3,7 @@ package de.keksuccino.loadmyresources;
 import de.keksuccino.loadmyresources.pack.LMRRepositorySource;
 import de.keksuccino.loadmyresources.pack.PackHandler;
 import de.keksuccino.loadmyresources.utils.config.Config;
+import net.minecraft.client.Minecraft;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.event.AddPackFindersEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -16,9 +17,9 @@ import java.io.File;
 @Mod("loadmyresources")
 public class LoadMyResources {
 
-    public static final String VERSION = "1.0.1";
-    
-    public static final File HOME_DIR = new File("config/loadmyresources/");
+    public static final String VERSION = "1.0.2";
+
+    public static final File HOME_DIR = new File(Minecraft.getInstance().gameDirectory, "config/loadmyresources/");
 
     public static final Logger LOGGER = LogManager.getLogger();
 
@@ -55,7 +56,7 @@ public class LoadMyResources {
 
         try {
 
-            config = new Config(HOME_DIR.getPath() + "/config.cfg");
+            config = new Config(HOME_DIR.getAbsolutePath() + "/config.cfg");
 
             //---------------------
 
