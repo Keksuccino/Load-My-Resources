2c69e1ddcf55e4b29eaed20f71c57483db73db9a
Keksuccino <35544624+Keksuccino@users.noreply.github.com>
2022-11-13 03:11:17 +0100

    Release v1.0.3

--- src/main/java/de/keksuccino/loadmyresources/LoadMyResources.java
+++ src/main/java/de/keksuccino/loadmyresources/LoadMyResources.java
@@ -15,9 +15,9 @@ import java.io.File;
 @Mod(modid = "loadmyresources", acceptedMinecraftVersions="[1.12,1.12.2]", dependencies = "required:forge@[14.23.5.2855,]", clientSideOnly = true)
 public class LoadMyResources {
 
-    public static final String VERSION = "1.0.2";
+    public static final String VERSION = "1.0.3";
 
-    public static final File HOME_DIR = new File(Minecraft.getMinecraft().mcDataDir, "config/loadmyresources/");
+    public static final File HOME_DIR = new File(getGameDirectory(), "config/loadmyresources/");
 
     public static final Logger LOGGER = LogManager.getLogger();
 
@@ -72,6 +72,14 @@ public class LoadMyResources {
             e.printStackTrace();
         }
 
+    }
+
+    public static File getGameDirectory() {
+        if (FMLClientHandler.instance().getSide() == Side.CLIENT) {
+            return Minecraft.getMinecraft().mcDataDir;
+        } else {
+            return new File("");
+        }
     }
 
 }
