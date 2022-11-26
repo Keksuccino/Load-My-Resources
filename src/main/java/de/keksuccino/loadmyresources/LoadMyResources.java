b936eade459fcd8a429693a7bccb2410fc233734
Keksuccino <35544624+Keksuccino@users.noreply.github.com>
2022-11-26 23:20:07 +0100

    Release v1.0.4

--- src/main/java/de/keksuccino/loadmyresources/LoadMyResources.java
+++ src/main/java/de/keksuccino/loadmyresources/LoadMyResources.java
@@ -15,7 +15,7 @@ import java.io.File;
 @Mod(modid = "loadmyresources", acceptedMinecraftVersions="[1.12,1.12.2]", dependencies = "required:forge@[14.23.5.2855,]", clientSideOnly = true)
 public class LoadMyResources {
 
-    public static final String VERSION = "1.0.3";
+    public static final String VERSION = "1.0.4";
 
     public static final File HOME_DIR = new File(getGameDirectory(), "config/loadmyresources/");
 
